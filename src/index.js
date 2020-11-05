6cb90622517f8babcb6229447941e678ff8d3e98
Shane O'Moore <shaneiadt@gmail.com>
2020-11-05 12:31:32 +0000

    make network request with axios

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('JavaScript is READY!');
+import { axios } from '@bundled-es-modules/axios';
+
+async function getQuote() {
+    try {
+        const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
+        const apiUrl = `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
+        const { data } = await axios.get(proxyUrl + apiUrl);
+        console.log(data);
+    } catch (error) {
+        getQuote();
+        console.error(error);
+    }
+}
+
+getQuote();
