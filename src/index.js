e70b73397a67077dcaccd7d73b17db6c96208a44
Shane O'Moore <shaneiadt@gmail.com>
2020-11-06 09:13:14 +0000

    add loader, update HTMl & xmlToJson function

--- src/index.js
+++ src/index.js
@@ -1,15 +1,62 @@
 import { axios } from '@bundled-es-modules/axios';
+import { xmlToJson } from './xmlToJson';
+
+const quoteContainer = document.querySelector('.quote-container');
+const quote = document.querySelector('#quote');
+const author = document.querySelector('#author');
+const twitterBtn = document.querySelector('#twitter');
+const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
+
+const apiUrl = `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
+let calls = 0;
+let delayFunctionCall = setTimeout(getQuote, 2000);
 
 async function getQuote() {
+    calls++;
+
     try {
-        const proxyUrl = `https://cors-anywhere.herokuapp.com/`;
-        const apiUrl = `http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json`;
+        const proxyUrl = `https://api.allorigins.win/get?url=${encodeURIComponent(apiUrl)}`;
         const { data } = await axios.get(proxyUrl + apiUrl);
-        console.log(data);
+
+        clearTimeout(delayFunctionCall);
+
+        const XmlNode = new DOMParser().parseFromString(data.contents, 'text/xml');
+        const json = xmlToJson(XmlNode);
+
+        const { forismatic: { quote: { quoteAuthor, quoteText, quoteLink } } } = json;
+
+        quote.textContent = quoteText;
+        author.textContent = typeof quoteAuthor != 'string' ? 'Unknown' : quoteAuthor;
+
+        loader.style.display = 'none';
+        quoteContainer.style.display = 'block';
+
+        if (quoteText.length > 120) {
+            quote.classList.add('long-quote');
+        } else {
+            quote.classList.remove('long-quote');
+        }
     } catch (error) {
-        getQuote();
+        // allow a max number of network requests calls
+        if (calls >= 5) {
+            clearTimeout(delayFunctionCall);
+        } else {
+            delayFunctionCall = setTimeout(getQuote, 2000);
+        }
         console.error(error);
     }
 }
 
-getQuote();
+function newQuote() {
+    loader.style.display = 'block';
+    quoteContainer.style.display = 'none';
+    delayFunctionCall = setTimeout(getQuote, 2000);
+}
+
+function tweet() {
+    window.open(`https://twitter.com/intent/tweet?text=${quote.textContent} - ${author.textContent}`, '_blank');
+}
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweet);
